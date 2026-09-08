5024c478800f6228eafc7f67a27ce72dce805ea3
Roham Roohnavaz <roohnavazroham12@gmail.com>
2026-09-08 23:36:45 +0330

    Start Repositories Implementations

---
+++ InspectionCenter.Repositories/MainRepositories/GenericRepo/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using InspectionCenter.Domain.Entities.Abstraction;
+using InspectionCenter.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectionCenter.Repositories.MainRepositories.GenericRepo
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly InspectionDbContext _dbContext;
+
+        public GenericRepository(InspectionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task AddAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InspectionCenter.Repositories/MainRepositories/GenericRepo/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectionCenter.Repositories.MainRepositories.GenericRepo
+{
+    public interface IGenericRepository<T>
+    {
+        Task<T> FindByNameAsync(string name);
+
+        Task AddAsync(T item);
+
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<List<T>> GetAllAsync();
+
+        Task UpdateAsync(Guid id); 
+
+        Task SoftDeleteAsync(Guid id);
+
+        Task HardDeleteAsync(Guid id);
+    }
+}
