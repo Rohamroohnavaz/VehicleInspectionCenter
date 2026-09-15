a6a0ac2b88161fa5dcb43d42483f25f8a8232ad4
Roham Roohnavaz <roohnavazroham12@gmail.com>
2026-09-15 21:40:25 +0330

    Implement WebAPI & MVC

---
+++ InspectionProjMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InspectionProjMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebLayer/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using InspectionCenter.Application.ServiceDtos;
+using InspectionCenter.Application.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebLayer.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("/AddCar")]
+        public async Task<IActionResult> AddCarWithChassisNumber([FromQuery] string chassisNumber)
+        {
+            var car = await _userService.AddCarsByChassisNumberAsync(chassisNumber);
+            return Ok(car);
+        }
+
+        [HttpPost("/ApplyAppointment")]
+        public async Task<IActionResult> ApplyAppointment([FromBody] CreateAppointmentDto dto)
+        {
+            var appointment = _userService.ApplyAppointmentAsync(dto);
+            return Ok(appointment);
+        }
+    }
+}
